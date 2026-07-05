cbfdfbba710a1b37d549732163d742741612342b
goatsongs <yaoxiwang@gmail.com>
2026-07-05 23:42:34 +0900

    Temporarily remove dataLayer

--- js/script.js
+++ js/script.js
@@ -1,9 +1,3 @@
 document.getElementById("addCart").addEventListener("click", function () {
     alert("カートに追加しました！");
 });
-
-dataLayer.push({
-  event: "add_to_cart",
-  item_name: "Tシャツ",
-  price: 3000
-});
